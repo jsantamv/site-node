150833cf199ed951daeba0983ff1fa365d20dd63
Juan Carlos Santamaria <jsantamaria@namutek.com>
2021-03-29 17:16:34 -0600

    Ejemplo de Angular

--- app.js
+++ app.js
@@ -13,30 +13,30 @@ hbs.registerPartials(__dirname + '/views/partials', function (err) {
 //Midleware, servir contenido estatico
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('home', {
-        nombre: 'Juan Carlos Santamaria',
-        curso: 'curso nodejs',
-    })
-})
+// app.get('/', (req, res) => {
+//     res.render('home', {
+//         nombre: 'Juan Carlos Santamaria',
+//         curso: 'curso nodejs',
+//     })
+// })
 
-app.get('/generic', (req, res) => {
-    res.render('generic', {
-        nombre: 'Juan Carlos Santamaria',
-        curso: 'curso nodejs',
-    })
-})
+// app.get('/generic', (req, res) => {
+//     res.render('generic', {
+//         nombre: 'Juan Carlos Santamaria',
+//         curso: 'curso nodejs',
+//     })
+// })
 
-app.get('/elements', (req, res) => {
-    res.render('elements', {
-        nombre: 'Juan Carlos Santamaria',
-        curso: 'curso nodejs',
-    })
-})
+// app.get('/elements', (req, res) => {
+//     res.render('elements', {
+//         nombre: 'Juan Carlos Santamaria',
+//         curso: 'curso nodejs',
+//     })
+// })
 
 //Comodin para validar si la pagina no existe
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 
